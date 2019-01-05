77e7f599832e9a838d76c604addfcc175edad44b
siegrain <siegrainwong@gmail.com>
2019-01-05 18:50:05 +0800

    improve project structure

--- k8s-demo/Services/NameService.cs
+++ k8s-demo/Services/NameService.cs
@@ -7,6 +7,7 @@ namespace k8s_demo.Services
 {
     public class NameService : INameService
     {
+        // 这个域名来源于 name-api 的 service 的 service name，通过这个地址就能让多个 service 进行内部访问
         private readonly string _nameUrl = "http://name-service/api/name";
         public async Task<string> GetName()
         {
